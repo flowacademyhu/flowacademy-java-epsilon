b754681c07598487d91844f97a8c14a672557f82
Attila Szegedi <szegedia@gmail.com>
2020-01-19 20:57:20 +0100

    Demonstrate use of fields in enums

--- src/main/java/hu/flowacademy/epsilon/_07_streams/Cards.java
+++ src/main/java/hu/flowacademy/epsilon/_07_streams/Cards.java
@@ -11,10 +11,29 @@ import java.util.stream.Stream;
  */
 public class Cards {
     public enum Suit {
-        HEARTS,
-        DIAMONDS,
-        SPADES,
-        CLUBS
+        HEARTS('\u2665', '\u2661'),
+        DIAMONDS('\u2666', '\u2662'),
+        SPADES('\u2660', '\u2664'),
+        CLUBS('\u2663', '\u2667');
+
+        // Enums can have fields
+        private final char blackSign;
+        private final char whiteSign;
+
+        // Fields are normally initialized in the constructor
+        // and constructors invoked when enumeration values
+        // are listed at the top of the enum.
+        Suit(char blackSign, char whiteSign) {
+            this.blackSign = blackSign;
+            this.whiteSign = whiteSign;
+        }
+
+        public char getBlackSign() {
+            return blackSign;
+        }
+        public char getWhiteSign() {
+            return whiteSign;
+        }
     }
 
     public enum Rank {
