85d2cc2b6ce10cc51137870af525d0509a770345
Attila Szegedi <szegedia@gmail.com>
2020-01-19 18:49:49 +0100

    Perform filtering already in the handler

--- src/main/java/hu/flowacademy/epsilon/_03_parsing/XmlSaxExample.java
+++ src/main/java/hu/flowacademy/epsilon/_03_parsing/XmlSaxExample.java
@@ -18,16 +18,29 @@ public class XmlSaxExample {
     // This is a SAX handler that builds Person objects whenever the SAX
     // parser encounters the start of an XML element named "person".
     private static final class PeopleHandler extends DefaultHandler {
+        PeopleHandler(int minAge) {
+            this.minAge = minAge;
+        }
+
+        final int minAge;
         final List<Person> people = new ArrayList<>();
 
         @Override
         public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
             if ("person".equals(qName)) {
-                people.add(new Person(
-                    attributes.getValue("firstName"),
-                    attributes.getValue("lastName"),
-                    1977 - Integer.parseInt(attributes.getValue("birthDate"))
-                ));
+                var age = 1977 - Integer.parseInt(attributes.getValue("birthDate"));
+                // By performing filtering already in the handler, we don't even construct
+                // those objects that wouldn't match the filter. When working with large
+                // datasets, moving processing into the handler and only keeping the minumum
+                // required data there can significantly reduce the amount of memory needed to
+                // run the processing.
+                if (age > minAge) {
+                    people.add(new Person(
+                        attributes.getValue("firstName"),
+                        attributes.getValue("lastName"),
+                        age
+                    ));
+                }
             }
         }
     }
@@ -35,7 +48,7 @@ public class XmlSaxExample {
     public static void loadXml() throws Exception {
         var parser = SAXParserFactory.newDefaultInstance().newSAXParser();
         try (var in = XmlSaxExample.class.getResourceAsStream("example1.xml")) {
-            var handler = new PeopleHandler();
+            var handler = new PeopleHandler(30);
             // You need to pass your handler to the parser. The handler needs to
             // build the necessary result as the parser calls its event handlers.
             parser.parse(in, handler);
@@ -45,7 +58,6 @@ public class XmlSaxExample {
             var people = handler.people;
 
             people.stream()
-                .filter(o -> o.age > 30)
                 .forEach(o -> System.out.println(String.format("%s %s", o.firstName, o.lastName)));
         }
     }
